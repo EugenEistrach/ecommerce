0c3282df1186053d4f8785f9b6c7d02866176294
Eugen Eistrach <eugen.eistrach@gmail.com>
2021-08-15 22:35:33 +0200

    #29 | Add Order & OrderItem Schema

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -4,10 +4,14 @@ import { CloudImage } from "./CloudImage";
 import { Product } from "./Product";
 import { Cart } from "./Cart";
 import { CartItem } from "./CartItem";
+import { Order } from "./Order";
+import { OrderItem } from "./OrderItem";
 
 export const lists = createSchema({
   User,
   Product,
+  Order,
+  OrderItem,
   Cart,
   CartItem,
   CloudImage,
